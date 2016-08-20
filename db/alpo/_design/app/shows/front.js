c066ee30540a424b7332a227f4ffcf57fb793133
Robin Millette <robin@millette.info>
2016-08-20 19:22:53 -0400

    more debug (this in show function).

--- db/alpo/_design/app/shows/front.js
+++ db/alpo/_design/app/shows/front.js
@@ -1,12 +1,11 @@
-(function () {
-  'use strict'
-
-  return function (doc, req) {
-    var ret = '<html><head><meta charset="utf-8"></head><body>'
+(() =>
+  (doc, req) => {
+    let ret = '<html><head><meta charset="utf-8"></head><body>'
 
     if (req.id && !doc) {
       ret += '<p>404, oups.</p>'
-      ret += '<pre>' + JSON.stringify(req, null, ' ') + '</pre>'
+      ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>`
+      ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
       return {
         code: 404,
         body: ret
@@ -14,8 +13,9 @@
     }
 
     ret += '<p>Hello monde</p>'
-    ret += '<pre>' + JSON.stringify(doc, null, ' ') + '</pre>'
-    ret += '<pre>' + JSON.stringify(req, null, ' ') + '</pre>'
+    ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>`
+    ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
+    ret += `<pre>${JSON.stringify(doc, null, ' ')}</pre>`
     return ret
   }
-}())
+)()
