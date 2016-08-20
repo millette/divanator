ee71cf712142ae6d6322402354f5d0568b1ad337
Robin Millette <robin@millette.info>
2016-08-20 19:25:04 -0400

    No more iife.

--- db/alpo/_design/app/shows/front.js
+++ db/alpo/_design/app/shows/front.js
@@ -1,21 +1,19 @@
-(() =>
-  (doc, req) => {
-    let ret = '<html><head><meta charset="utf-8"></head><body>'
+(doc, req) => {
+  let ret = '<html><head><meta charset="utf-8"></head><body>'
 
-    if (req.id && !doc) {
-      ret += '<p>404, oups.</p>'
-      ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>`
-      ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
-      return {
-        code: 404,
-        body: ret
-      }
-    }
-
-    ret += '<p>Hello monde</p>'
+  if (req.id && !doc) {
+    ret += '<p>404, oups.</p>'
     ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>`
     ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
-    ret += `<pre>${JSON.stringify(doc, null, ' ')}</pre>`
-    return ret
+    return {
+      code: 404,
+      body: ret
+    }
   }
-)()
+
+  ret += '<p>Hello monde</p>'
+  ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>`
+  ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
+  ret += `<pre>${JSON.stringify(doc, null, ' ')}</pre>`
+  return ret
+}
