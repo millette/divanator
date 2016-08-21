78e0a8aa489653e2871be9dba8f86ef0fe32ee1a
Robin Millette <robin@millette.info>
2016-08-20 23:44:29 -0400

    Directory argument.

--- bab.js
+++ bab.js
@@ -1,3 +1,6 @@
+// core
+const path = require('path')
+
 // npm
 const UglifyJS = require('uglify-js')
 const babel = require('babel-core')
@@ -13,33 +16,33 @@ const divanatorFile = (() => {
     return (str) => repout(UglifyJS.minify(repin(str), { fromString: true }).code)
   })()
   const transform = (fn, resolve, reject) => babel.transformFile(
-    fn,
-    { presets: ['es2015'] },
+    fn, { presets: ['es2015'] },
     (err, ok) => err ? reject(new Error(err)) : resolve(minjs(noiife(ok)))
   )
   return (fn) => new Promise(transform.bind(null, fn))
 })()
 
+const divanator = (ddocPath) => {
+  const resolve = path.resolve
+  const lp = path.basename(ddocPath)
+  return Promise.all([
+    divanatorFile(resolve(ddocPath, 'shows/front.js')),
+    divanatorFile(resolve(ddocPath, 'views/i1/map.js'))
+  ])
+    .then((x) => {
+      return {
+        _id: '_design/' + lp,
+        rewrites: require(resolve(ddocPath, 'rewrites.json')),
+        language: 'javascript',
+        shows: { front: x[0] },
+        views: { i1: { map: x[1], reduce: '_count' } }
+      }
+    })
+}
+
 const jsonlog = (x) => console.log(JSON.stringify(x, null, ' '))
 
-Promise.all([
-  divanatorFile('db/alpo/_design/app/shows/front.js'),
-  divanatorFile('db/alpo/_design/app/views/i1/map.js')
-])
-  .then((x) => {
-    return {
-      _id: '_design/app',
-      language: 'javascript',
-      shows: {
-        front: x[0]
-      },
-      views: {
-        i1: {
-          map: x[1],
-          reduce: '_count'
-        }
-      }
-    }
-  })
+// divanator('.')
+divanator('ddoc/app')
   .then(jsonlog)
   .catch(console.log)
