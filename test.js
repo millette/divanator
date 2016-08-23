59b011e87ac29e196e0f7d3bb2d96a47d07c9d49
Robin Millette <robin@millette.info>
2016-08-23 03:43:11 -0400

    db write test.

--- test.js
+++ test.js
@@ -10,5 +10,10 @@ test('go', async t => {
 test('not a ddoc', async t => await t.throws(fn('ddoc/app666'), 'Are you sure ddoc/app666 is a design doc?'))
 
 if (process.env.MEVLA_COUCHDB) {
+  test('db', async t => {
+    const result = await fn('ddoc/app', 'http://localhost:5984/bobo')
+    t.is(result.id, '_design/app')
+  })
+
   test('not a db', async t => await t.throws(fn('ddoc/app', 'http://localhost:5984/notexist'), 'no_db_file'))
 }
