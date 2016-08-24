74fef92943fb76400601928a311a81228d0fc1df
Robin Millette <robin@millette.info>
2016-08-23 23:44:17 -0400

    Unit testing couchdb code.

---
+++ test-ddoc.js
@@ -0,0 +1,22 @@
+'use strict'
+import test from 'ava'
+import fn from './ddoc/app/shows/front'
+import fn2 from './ddoc/app/views/i1/map'
+
+test('show front', t => {
+  const doc = {}
+  const req = {}
+  const result = fn(doc, req)
+  t.is(result, '<html><head><meta charset="utf-8"></head><body><p>Hello monde</p><pre>{}</pre><pre>{}</pre>')
+})
+
+test('view il', t => {
+  const doc = { _id: 'le-id', _rev: '1-abc' }
+  t.plan(2)
+  fn2(doc, {
+    emit: (k, v) => {
+      t.is(k, 'le-id')
+      t.is(v, '1-abc')
+    }
+  })
+})
