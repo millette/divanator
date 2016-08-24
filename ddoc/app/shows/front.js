585835c0b5c153c00ec10e8b528e83e154407faa
Robin Millette <robin@millette.info>
2016-08-24 17:38:22 -0400

    Never mind this.

--- ddoc/app/shows/front.js
+++ ddoc/app/shows/front.js
@@ -4,16 +4,13 @@ module.exports = function (doc, req) {
 
   if (req.id && !doc) {
     ret += '<p>404, oups.</p>'
-    if (this) { ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>` }
+    // if (this) { ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>` }
     ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
-    return {
-      code: 404,
-      body: ret
-    }
+    return { code: 404, body: ret }
   }
 
   ret += '<p>Hello monde</p>'
-  if (this) { ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>` }
+  // if (this) { ret += `<pre>${JSON.stringify(this, null, ' ')}</pre>` }
   ret += `<pre>${JSON.stringify(req, null, ' ')}</pre>`
   ret += `<pre>${JSON.stringify(doc, null, ' ')}</pre>`
   return ret
